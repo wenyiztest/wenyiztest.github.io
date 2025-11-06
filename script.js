756c50a8140a2589ff06d290f76bc41776a2442f
wenyiztest <wenyiztest@gmail.com>
2025-11-06 10:54:17 -0500

    Initial clean commit of Wenyi's portfolio page

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+// JavaScript statement that outputs the string "Welcome to Wenyi's Page!" to the browser's developer console.
+// This is a common method for debugging and displaying information during development.
+console.log("Welcome to Wenyi's Page!");
+
